611f4562a73097bf9699fcee15626ff53f8a5374
LWR <lwr1104@qq.com>
2025-06-18 21:15:24 +0800

    feat: 新增配置类

---
+++ src/main/java/com/starlwr/bot/adapter/napcat/config/StarBotNapCatAdapterProperties.java
@@ -0,0 +1,32 @@
+package com.starlwr.bot.adapter.napcat.config;
+
+import com.starlwr.bot.core.plugin.StarBotComponent;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * StarBotNapCatAdapter 配置类
+ */
+@Getter
+@Setter
+@Configuration
+@StarBotComponent
+@ConfigurationProperties(prefix = "starbot.adapter.napcat")
+public class StarBotNapCatAdapterProperties {
+    /**
+     * 接口地址
+     */
+    private String address = "localhost";
+
+    /**
+     * 端口号
+     */
+    private int port = 3000;
+
+    /**
+     * Token
+     */
+    private String token;
+}
