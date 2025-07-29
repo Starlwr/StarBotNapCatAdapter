325600d03705eee16535a944f8444110ec2414d6
LWR <lwr1104@qq.com>
2025-07-29 19:56:54 +0800

    fix: 修复推送 API 被注册到默认 8080 端口的问题

--- src/main/java/com/starlwr/bot/adapter/onebot/controller/OneBotController.java
+++ src/main/java/com/starlwr/bot/adapter/onebot/controller/OneBotController.java
@@ -9,10 +9,13 @@ import com.starlwr.bot.core.model.Message;
 import com.starlwr.bot.core.model.Sender;
 import com.starlwr.bot.core.plugin.StarBotComponent;
 import com.starlwr.bot.core.service.StarBotSenderService;
-import jakarta.annotation.PostConstruct;
 import jakarta.annotation.Resource;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.context.WebServerApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,9 +28,10 @@ import java.lang.reflect.Method;
  * OneBot 控制器
  */
 @Slf4j
+@Order(-20000)
 @RestController
 @StarBotComponent
-public class OneBotController {
+public class OneBotController implements ApplicationListener<ApplicationReadyEvent> {
     @Resource
     private WebServerApplicationContext webContext;
 
@@ -46,9 +50,14 @@ public class OneBotController {
     @Resource
     private OneBotWebsocketService websocketService;
 
-    @PostConstruct
-    public void init() throws NoSuchMethodException {
-        Method method = getClass().getMethod("send", Message.class);
+    @Override
+    public void onApplicationEvent(@NonNull ApplicationReadyEvent event) {
+        Method method;
+        try {
+            method = getClass().getMethod("send", Message.class);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("注册推送 API 异常", e);
+        }
 
         for (OneBotSender sender : properties.getSenders()) {
             try {
@@ -77,5 +86,10 @@ public class OneBotController {
      */
     public JSONObject send(@RequestBody Message message) {
         return httpService.send(message);
+    }
+
+    @Override
+    public boolean supportsAsyncExecution() {
+        return false;
     }
 }
